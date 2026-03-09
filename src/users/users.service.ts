ea6777112e9562254cf321b82e423bd3cd65158d
a-hazza22 <7483088@ogrenci.bilecik.edu.tr>
2026-03-09 22:47:17 +0300

    add users api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,23 +4,42 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  private users = [] as any[];
+  private idCounter = 1;
+  
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = {
+      id: this.idCounter++, ...createUserDto
+    };
+    this.users.push(user);
+
+    return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
+    
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.users.find(user => user.id === id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: Partial<UpdateUserDto>) {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) {
+      return 'user with id ${id} not found';
+    }
+    this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+    return this.users[userIndex];
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (userIndex === -1) {
+      return 'user with id ${id} not found';
+    }
+    this.users.splice(userIndex, 1);
+    return 'This action remove a #${id} user';
   }
 }
